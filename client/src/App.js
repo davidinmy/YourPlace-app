d64f162718df04a55bc73f89dca3473842ff987c
David He <heliushanchuan@gmail.com>
2023-09-12 17:20:12 +0800

    add userItem component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // element: <Users />,
+    children: [
+      { index: true, element: <Users /> },
+      {
+        path: "places/new",
+        element: <NewPlace />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ client/src/users/pages/Users.js
@@ -0,0 +1,18 @@
+import React from "react";
+import UsersList from "../components/UsersList";
+
+const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Josh",
+      image:
+        "https://lh3.googleusercontent.com/ulTNkL1s8SxAEkwH4pxkHxMYh2r5HJeBRcURychonbbelmY7Yg1mFkJUS_toAPVTTBGqDtuc1dq5ZwsJv9fUQ0h12f5M919x10wmaUU",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
