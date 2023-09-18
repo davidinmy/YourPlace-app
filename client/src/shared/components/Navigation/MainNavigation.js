1b9b6597f9dc4ca425304a7ffb9d16431506401f
David He <heliushanchuan@gmail.com>
2023-09-18 22:00:15 +0800

    Sidedrawer animation

--- client/src/shared/components/Navigation/MainNavigation.js
+++ client/src/shared/components/Navigation/MainNavigation.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
-
-import { Link } from "react-router-dom";
+import Backdrop from "../UIElements/Backdrop";
 import "./MainNavigation.css";
 
-export const MainNavigation = (props) => {
+const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
   return (
-    <>
-      <SideDrawer>
+    <React.Fragment>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
       </SideDrawer>
+
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
           <span />
           <span />
           <span />
@@ -27,7 +43,7 @@ export const MainNavigation = (props) => {
           <NavLinks />
         </nav>
       </MainHeader>
-    </>
+    </React.Fragment>
   );
 };
 
