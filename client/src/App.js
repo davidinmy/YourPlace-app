2c2c11ecf02e675e18031994011e0ec3a41c9760
David He <heliushanchuan@gmail.com>
2023-09-19 15:58:00 +0800

    Add formElement Button and Place list & items

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Users from "./users/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
 import RootLayout from "./shared/components/Navigation/Root";
 
 const router = createBrowserRouter([
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
     id: "root",
     children: [
       { index: true, element: <Users /> },
+      {
+        path: ":userId/places",
+        element: <UserPlaces />,
+      },
       {
         path: "places/new",
         element: <NewPlace />,
